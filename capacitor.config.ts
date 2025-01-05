a27d63088192559d2bea2941ebd6dfdded399c38
Saud <saudkhan03@outlook.com>
2025-01-05 11:30:09 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Masjid near me',
+  webDir: 'www'
+};
+
+export default config;
