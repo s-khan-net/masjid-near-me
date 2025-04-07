b1e3bc6a385831ddd1e1c7b90bcd17f639a2aa60
Saud <saudkhan03@outlook.com>
2025-04-07 15:10:59 +0530

    Refactor settings update logic to improve error handling and reset location after successful update

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -56,19 +56,45 @@ export class SettingsComponent implements OnInit {
       );
       this._settingsService
         .updateSettings(this.settings, SettingType.SETTINGS)
-        .then((res) => {
-          if (res) {
-            this._loaderService.hideLoader();
-            this._loaderService.LoaderMessage = 'Settings updated successfully';
-            this._loaderService.ShowSpinner = false;
-            this._loaderService.showLoader();
-            this.hide();
-            setTimeout(() => {
+        .then(
+          (res) => {
+            if (res) {
               this._loaderService.hideLoader();
-              this._locationService.resetLocation();
-            }, 4500);
+              this._loaderService.LoaderMessage =
+                'Settings updated successfully';
+              this._loaderService.ShowSpinner = false;
+              this._loaderService.showLoader();
+              this.hide();
+              setTimeout(() => {
+                this._loaderService.hideLoader();
+                this._locationService.resetLocation();
+              }, 4500);
+            }
+          },
+          (err) => {
+            if (this._accessdeniedError(err)) return;
           }
-        });
+        );
     }
+  }
+
+  private _accessdeniedError(err: any): boolean {
+    if (
+      err.status === 401 ||
+      (typeof err.error == 'string' &&
+        err.error?.toLowerCase().indexOf('access denied') > -1)
+    ) {
+      this._loaderService.hideLoader();
+      this._popupService.closePopups();
+      this._loaderService.LoaderMessage =
+        'it has been a while since you have logged in. Please log in again.';
+      this._loaderService.ShowSpinner = false;
+      this._loaderService.showLoader();
+      setTimeout(() => {
+        this._loaderService.hideLoader();
+      }, 5000);
+      return true;
+    }
+    return false;
   }
 }
